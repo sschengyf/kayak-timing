a93757a989ccfd32058bb4339680568896483e5a
Ivan Cheng <sschengyf@gmail.com>
2022-09-06 21:18:25 +1200

    chore(debug): only get tides

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import * as dotenv from "dotenv";
-import { getWeatherForecast } from "./weatherForecast";
+// import { getWeatherForecast } from "./weatherForecast";
 import { getTideForecast } from "./tideForecast";
 
 dotenv.config();
 
-async function predictSuitableKayakTimes() {
-  const weatherForecastData = await getWeatherForecast();
-  console.log(weatherForecastData);
-}
+// async function predictSuitableKayakTimes() {
+// const weatherForecastData = await getWeatherForecast();
+// console.log(weatherForecastData);
+// }
 
-predictSuitableKayakTimes();
+// predictSuitableKayakTimes();
 
 async function predictTides() {
   const tides = await getTideForecast();
