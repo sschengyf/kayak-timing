d7c18ae9a1b93f6c1ad189d1d360d4a8cafcd616
Ivan Cheng <sschengyf@gmail.com>
2022-09-08 22:17:55 +1200

    refactor: remove async keyword

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { getTideForecast, onlyHighTide } from "./tideForecast";
 
 dotenv.config();
 
-async function predictSuitableKayakTimes() {
+function predictSuitableKayakTimes() {
   const weatherForecast = from(getWeatherForecast());
   const highTideForecast = from(getTideForecast({ lat: "-36.8329145419162", long: "174.79377033086192" })).pipe(
     map((value) => value && onlyHighTide(value))
